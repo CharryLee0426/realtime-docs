dff17f2dbc14a420171f47a5bc55e240b4c43ae6
Charry Lee <lichen000426@gmail.com>
2024-12-05 11:46:14 -0800

    Document update and delete functionalities; document table ui design complete

--- src/app/(home)/document-row.tsx
+++ src/app/(home)/document-row.tsx
@@ -1,17 +1,26 @@
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Doc } from "../../../convex/_generated/dataModel";
 import { SiGoogledocs } from "react-icons/si";
-import { Building2Icon, CircleUserIcon, MoreVertical } from "lucide-react";
+import { Building2Icon, CircleUserIcon } from "lucide-react";
 import { format } from "date-fns";
-import { Button } from "@/components/ui/button";
+import { DocumentMenu } from "./document-menu";
+import { useRouter } from "next/navigation";
 
 interface DocumentRowProps {
     document: Doc<"documents">;
 }
 
 export const DocumentRow = ({ document }: DocumentRowProps) => {
+    const router = useRouter();
+    const onNewTabClick = (id: string) => {
+        window.open(`/documents/${id}`, "_blank");
+    }
+    const onRowClick = (id: string) => {
+        router.push(`/documents/${id}`);
+    }
+
     return (
-        <TableRow key={document._id} className="cursor-pointer">
+        <TableRow onClick={() => onRowClick(document._id)} key={document._id} className="cursor-pointer">
             <TableCell className="w-[50px]">
                 <SiGoogledocs className="size-6 fill-blue-500"/>
             </TableCell>
@@ -26,9 +35,11 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
                 {format(new Date(document._creationTime), "MMM dd, yyyy")}
             </TableCell>
             <TableCell className="flex justify-end">
-                <Button variant="ghost" size="icon" className="rounded-full">
-                    <MoreVertical className="size-4"/>
-                </Button>
+                <DocumentMenu 
+                    documentId={document._id}
+                    title={document.title}
+                    onNewTab={onNewTabClick}
+                />
             </TableCell>
         </TableRow>
     )
