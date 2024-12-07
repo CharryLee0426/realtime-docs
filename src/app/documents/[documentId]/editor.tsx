357bae16d9fb1e3b50edfbc2f4b38e7b015eb19e
Charry Lee <lichen000426@gmail.com>
2024-12-07 00:44:29 -0800

    Margin ruler functionality

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -26,8 +26,11 @@ import { LineHeightExtension } from '@/extensions/line-height';
 import { useEditorStore } from '@/app/store/use-editor-store'
 import { Ruler } from './ruler';
 import { Threads } from "./threads";
+import { useStorage } from "@liveblocks/react";
 
 export const Editor = () => {
+    const leftMargin = useStorage((root) => root.leftMargin);
+    const rightMargin = useStorage((root) => root.rightMargin);
     const liveblocks = useLiveblocksExtension();
     const { setEditor } = useEditorStore();
 
@@ -59,7 +62,7 @@ export const Editor = () => {
         },
         editorProps: {
             attributes: {
-                style: "padding-left: 56px; padding-right: 56px;", // Added padding to the editor
+                style: `padding-left: ${leftMargin ?? 56}px; padding-right: ${rightMargin ?? 56}px;`, // Added padding to the editor
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text", // making the editor like a doc
                 spellcheck: "true", // Explicitly set the spellcheck attribute
             },
